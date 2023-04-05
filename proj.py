9e895f3cb0f350d2b1922a5ab27d349c19c6f308
Dylan-T-Peterson <dylantpeterson@yahoo.com>
2023-04-04 22:04:36 -0800

    made changes to profit chart, edited structure

--- proj.py
+++ proj.py
@@ -13,15 +13,35 @@ df.Sales = df.Sales.round(2)
 df['Profit Margin'] = ( (df.Profit / df.Sales) * 100 ).round(2)
 df['Lead Time'] = df['Ship Date'] - df['Order Date']
 
+def endlabel(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width() / 2, height + 1,
+        height,
+        ha='center', va='bottom', color='white')
+
+
 def profit_chart():
-    plt.subplot(2,2,1)
-    x = df.groupby(by='Sub-Category').Sales.mean()
-    sns.barplot(data=df, x='Sales', y='Sub-Category', hue='Category')
+    fig, ax = plt.subplots(2,2)
+    #x = df.groupby(by='Sub-Category').Sales.mean()
+    ax[0,0] = sns.barplot(
+    data=df, x='Sales', y='Sub-Category', hue='Category',
+    estimator='sum', errorbar=None,
+    dodge=False)
+    plt.xlabel('Sales (in USD)')
 
-    plt.subplot(2,2,2)
-    sns.barplot(data=df, x='Profit', y='Sub-Category', hue='Category')
+    #plt.subplot(2,2,2)
+    ax[0,1] = sns.barplot(data=df, x='Profit', y='Sub-Category', hue='Category',
+    estimator='sum', errorbar=None,
+    dodge=False)
+    plt.xlabel('Profit (in USD)')
 
-    plt.subplot(2,1,2)
-    sns.barplot(data=df, x='Profit Margin', y='Sub-Category', hue='Category')
+    fig.subplots(2,1,2)
+    ax[1, 0:] = sns.barplot(data=df, x='Profit Margin', y='Sub-Category', hue='Category',
+    errorbar=None, estimator='mean',
+    dodge=False)
+    plt.xlabel('Profit Margins (Percent)')
+    
+    plt.show()
 if __name__ == '__main__':
-    profit_chart()##
+    profit_chart()
